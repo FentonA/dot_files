30ca5b5d2e320d50c2dc3e36ba7843de2ec094dd
Alf Fenton <Fentonalf@gmail.com>
2024-11-30 13:12:51 -0600

    Adds extra plugins, and updates the clickk tmuxinator

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -1,12 +1,17 @@
--- lua/custom/plugins/lint.lua
-
 return {
   {
-    -- Linting Plugin
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
+
+      -- Define Clippy as a custom linter
+      lint.linters.clippy = {
+        cmd = 'cargo',
+        args = { 'clippy', '--message-format=json' },
+        stdin = false,
+        append_fname = false,
+      }
 
       -- Define linters for specific filetypes
       lint.linters_by_ft = {
@@ -14,20 +19,21 @@ return {
         lua = { 'luacheck' },
         python = { 'flake8' },
         ruby = { 'rubocop' },
-        rust = { 'rustfmt', 'clippy' },
+        rust = { 'clippy' },
         typescript = { 'eslint' },
         javascript = { 'eslint' },
-        -- Add more filetypes and their linters as needed
       }
 
       -- Create an autocommand group for linting
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      local debounce = require('plenary.async.util').debounce
+      local debounced_lint = debounce(500, lint.try_lint)
 
       -- Set up autocommands to trigger linting
       vim.api.nvim_create_autocmd({ 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
-          local success, err = pcall(lint.try_lint)
+          local success, err = pcall(debounced_lint)
           if not success then
             vim.notify('Linting failed: ' .. err, vim.log.levels.ERROR)
           end
