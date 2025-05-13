eb22bedc257de8eee38e7d47b956bf22ff8f46d4
Alf Fenton <Fentonalf@gmail.com>
2025-05-13 09:08:24 -0500

    Completes avante implementation

--- nvim/lua/kickstart/plugins/lazy.lua
+++ nvim/lua/kickstart/plugins/lazy.lua
@@ -491,8 +491,8 @@ return {
     require 'kickstart.plugins.lint',
     require 'kickstart.plugins.autopairs',
     require 'kickstart.plugins.neo-tree',
-    require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
+    require 'kickstart.plugins.gitsigns', 
+    require 'kickstart.plugins.avante', 
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    This is the easiest way to modularize your config.
     --
