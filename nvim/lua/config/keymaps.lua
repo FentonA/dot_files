60992d7ac5795476d1ca6f0f950c69078ec81c1a
Alf Fenton <Fentonalf@gmail.com>
2026-03-10 15:36:18 +0700

    Adds updates to ghostty Adds claude code to nvim Refactos tmuxinator

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Clickk test runners
+vim.keymap.set("n", "<leader>tu", "<cmd>split | terminal just test-user<cr>", { desc = "Test user-service" })
+vim.keymap.set("n", "<leader>tc", "<cmd>split | terminal just test-content<cr>", { desc = "Test content-service" })
+vim.keymap.set("n", "<leader>te", "<cmd>split | terminal just test-engagement<cr>", { desc = "Test engagement-service" })
+vim.keymap.set("n", "<leader>ta", "<cmd>split | terminal just test<cr>", { desc = "Test all services" })
